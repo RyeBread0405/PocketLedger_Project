1c4755ff9a130b4ad3c947faae16bdcfc59098e9
Sofia Robles <sofia.robles@okstate.edu>
2026-09-06 14:03:14 -0500

    adding spring initializr project file for backend management

---
+++ Backend/pocketledger/src/test/java/edu/okstate/pocketledger/PocketledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.okstate.pocketledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
