9be452c03e03f4889f4da1efe470820a59a551e0
Sofia Robles <sofianrobles@gmail.com>
2026-09-18 10:02:31 -0500

    removing unused library import

--- Backend/pocketledger/src/main/java/edu/okstate/pocketledger/account/CreateAccountRequest.java
+++ Backend/pocketledger/src/main/java/edu/okstate/pocketledger/account/CreateAccountRequest.java
@@ -2,7 +2,6 @@ package edu.okstate.pocketledger.account;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateAccountRequest(
